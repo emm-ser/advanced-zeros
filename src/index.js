73a3f041ad07f150fcad46b6bfad6ff433add8fa
emm-ser <106416@bk.ru>
2018-03-02 23:02:16 +0300

    advanced-zeros/bag-fixid: add func to find prime factors

--- src/index.js
+++ src/index.js
@@ -1,27 +1,57 @@
-module.exports = function getZerosCount(number, base) {
-  var zeros = Number.MAX_VALUE;
+// Решение состоит из 2 этапов.
+// Количество нулей будет равняться наименьшему наименьшему часному от
+// количества множителей (этап 2) и количества простых множителей (этап 1)
 
-    for (var i = 2; i <= base; i++) {
-        if (base % i == 0) {
-            amountOfBaseMultiplier = baseMultiplier(base, i);
-            amountOfNumberMultiplier = numberMultiplier(number, i);
-            zeros = Math.min(zeros, amountOfNumberMultiplier / amountOfBaseMultiplier) ^ 0;
-        }
+module.exports = function getZerosCount(number, base) {
+
+    var zeros = number;
+    var factors = findFactors(base);
+
+    for(let i of factors.keys()) {
+        var amountOfNumberMultiplier = numberMultiplier(number, i);
+        zeros = Math.min(zeros, amountOfNumberMultiplier / factors.get(i)) ^ 0;
     }
+
     return zeros;
 }
 
-function baseMultiplier(base, i) {
-    amount = 0;
-    while (base % i == 0) {
-        base = (base / i) ^ 0;
-        amount++;
+// Этап 1.
+// Разложение системы счисления(base) на простые множители.
+// Результатом будет Map, где key -- множитель, а value -- кол-во свхождений key.
+function findFactors(number) {
+    var factors = new Map();
+    var count = 0;
+
+    while (number % 2 == 0) {
+        count++;
+        number /= 2;
     }
-    return amount;
+    if (count > 0) {
+        factors.set(2, count);
+    }
+
+    for (var i = 3; i*i <= number; i = i + 2) {
+        count = 0;
+        while(number % i == 0) {
+            count++;
+            number /= i;
+        }
+        if (count > 0) {
+            factors.set(i, count);
+        }
+    }
+
+    if(number > 1) {
+        factors.set(number, 1);
+    }
+
+    return factors;
 }
 
+// Этап 2.
+// Нахождение числа множителей числа i для факториала числа number
 function numberMultiplier(number, i) {
-    amount = 0;
+    var amount = 0;
     var reminder = number;
     while (true) {
         reminder = (reminder / i) ^ 0;
@@ -33,3 +63,5 @@ function numberMultiplier(number, i) {
     }
     return amount;
 }
+
+
