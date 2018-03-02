dd80817856981f46d18bb753574c4108930d8bb1
emm-ser <106416@bk.ru>
2018-03-02 15:11:19 +0200

    advanced-zeros/feature: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var zeros = Number.MAX_VALUE;
+
+    for (var i = 2; i <= base; i++) {
+        if (base % i == 0) {
+            amountOfBaseMultiplier = baseMultiplier(base, i);
+            amountOfNumberMultiplier = numberMultiplier(number, i);
+            zeros = Math.min(zeros, amountOfNumberMultiplier / amountOfBaseMultiplier) ^ 0;
+        }
+    }
+    return zeros;
+}
+
+function baseMultiplier(base, i) {
+    amount = 0;
+    while (base % i == 0) {
+        base = (base / i) ^ 0;
+        amount++;
+    }
+    return amount;
+}
+
+function numberMultiplier(number, i) {
+    amount = 0;
+    var reminder = number;
+    while (true) {
+        reminder = (reminder / i) ^ 0;
+        if (reminder >= 1) {
+            amount += reminder;
+        } else {
+            break;
+        }
+    }
+    return amount;
 }
